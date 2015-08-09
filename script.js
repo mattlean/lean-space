7a4aa2998f45a7d1e276b8535b0362836a200807
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-08-08 22:51:48 -0700

    Progress on mobile menu

---
+++ script.js
@@ -0,0 +1,14 @@
+var btnMobileMenu = document.getElementById('btn-mobile-menu');
+var mobileMenu = document.getElementById('mobile-menu');
+
+function toggleMobileMenu() {
+	smmClassName = 'show-mobile-menu';
+
+	if(mobileMenu.className === smmClassName) {
+		mobileMenu.className = '';
+	} else {
+		mobileMenu.className = smmClassName;
+	}
+}
+
+btnMobileMenu.addEventListener('click', toggleMobileMenu);
