cd4d5adde6659a9be5b76210559df99eb367f3b3
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-08-10 21:33:56 -0700

    Adds back button functionality

--- script.js
+++ script.js
@@ -13,11 +13,10 @@ function toggleMobileMenu() {
 
 function getPath(href) {
 	var splitHref = href.split('/');
-	var path = '';
+	var path = '/';
 
 	// i starts at 3 because that is after the hostname in splitHref
 	for(var i = 3; i < splitHref.length; ++i) {
-		console.log(splitHref[i]);
 		path += splitHref[i];
 
 		if((i + 1) < splitHref.length) {
@@ -25,22 +24,15 @@ function getPath(href) {
 		}
 	}
 
+	console.log('get path called');
 	return path;
 }
 
-function linkMod(link) {
-	link.addEventListener('click', function(e) {
-		swapContent(link.href);
-		history.pushState(null, null, link.href);
-		e.preventDefault();
-	}, false);
-}
-
-function swapContent(href) {
+/* Swap content without page refresh */
+function swapContent(path) {
 	var req = new XMLHttpRequest();
-	var path = getPath(href);
-	console.log(path);
 
+	console.log('Downloading "' + path + '".');
 	req.open('GET',
 			path,
 			false
@@ -48,13 +40,28 @@ function swapContent(href) {
 	req.send();
 
 	if(req.status === 200) {
-		console.log('success');
+		console.log('Download succeeded.');
 		//console.log(req.responseText);
 		document.getElementById('content').innerHTML = req.responseText;
 		return true;
 	}
-	console.log('nope');
+	console.log('Download failed.');
 	return false;
+}
+
+/* Cover back button case */
+window.addEventListener('popstate', function(e) {
+	console.log(location.pathname);
+	swapContent(location.pathname);
+});
+
+function linkMod(link) {
+	link.addEventListener('click', function(e) {
+		var path = getPath(link.href);
+		swapContent(path);
+		history.pushState(null, null, link.href);
+		e.preventDefault();
+	}, false);
 }
 
 /* Main */
