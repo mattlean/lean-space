42c3fe8a5eb3c04d6cc663ca046e532244811fa0
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-08-10 21:11:10 -0700

    Experiments with history API

--- script.js
+++ script.js
@@ -11,22 +11,55 @@ function toggleMobileMenu() {
 	}
 }
 
+function getPath(href) {
+	var splitHref = href.split('/');
+	var path = '';
+
+	// i starts at 3 because that is after the hostname in splitHref
+	for(var i = 3; i < splitHref.length; ++i) {
+		console.log(splitHref[i]);
+		path += splitHref[i];
+
+		if((i + 1) < splitHref.length) {
+			path += '/';
+		}
+	}
+
+	return path;
+}
+
 function linkMod(link) {
 	link.addEventListener('click', function(e) {
-		console.log(location.hostname);
-		//history.pushState(null, null, link.href);
+		swapContent(link.href);
+		history.pushState(null, null, link.href);
 		e.preventDefault();
 	}, false);
 }
 
-/*function swapContent(href) {
+function swapContent(href) {
 	var req = new XMLHttpRequest();
+	var path = getPath(href);
+	console.log(path);
+
 	req.open('GET',
-			location.hostname + '';
+			path,
+			false
 		);
-}*/
+	req.send();
+
+	if(req.status === 200) {
+		console.log('success');
+		//console.log(req.responseText);
+		document.getElementById('content').innerHTML = req.responseText;
+		return true;
+	}
+	console.log('nope');
+	return false;
+}
 
 /* Main */
 btnMobileMenu.addEventListener('click', toggleMobileMenu);
 
 linkMod(document.getElementById('test'));
+linkMod(document.getElementById('test2'));
+linkMod(document.getElementById('test3'));
