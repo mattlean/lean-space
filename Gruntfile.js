1bb6d52eb50fb447dba5ce378a990bea3fa2a54c
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-08-30 08:14:44 -0700

    Fixes lb close transition bug, sets Gruntfile to new file structure, new imgs

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 			options: {
 				force: true
 			},
-			all: ['src/index.html', 'src/about.html', 'src/contact.html', 'src/work.html', 'src/resume.html', 'src/projectpage.html', 'src/blog.html', 'src/blogpost.html']
+			all: ['src/*.html']
 		},
 		sass: {
 			dev: {
